861a736eaf0db0ebb362b5a54e60232e0870471a
Alex Potemkin <koldunpot98@gmail.com>
2024-10-10 21:37:26 +0700

    Добавление в файл возможности проверки функции get_date

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from src.widget import mask_account_card, get_date
+from src.widget import get_date, mask_account_card
 
 if __name__ == "__main__":
     input_str = input("Введите данные карты или счёта: ")
