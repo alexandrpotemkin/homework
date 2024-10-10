20b917628721e3b3f4a1ba8f3f83141d9e810499
Alex Potemkin <koldunpot98@gmail.com>
2024-10-10 21:18:44 +0700

    Добавление в файл возможности проверки функции get_date

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from src.widget import mask_account_card
+from src.widget import mask_account_card, get_date
 
 if __name__ == "__main__":
     input_str = input("Введите данные карты или счёта: ")
     print(mask_account_card(input_str))
+
+    input_str = input("Введите дату в формате 'YYYY-MM-DDTHH:MM:SS.SSSSSS': ")
+    print(get_date(input_str))
