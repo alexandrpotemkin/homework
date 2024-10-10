00baefdbb85deb3820544e7cb3a4dccad32c3835
Alex Potemkin <koldunpot98@gmail.com>
2024-10-10 21:15:18 +0700

    Добавление функции изменения формата даты

--- src/widget.py
+++ src/widget.py
@@ -21,3 +21,22 @@ def mask_account_card(input_str: str) -> str:
         # Обработка карты
         card_number = input_str.split()[-1]
         return input_str.replace(card_number, get_mask_card_number(card_number))
+
+
+from datetime import datetime
+
+
+def get_date(date_str: str) -> str:
+    """
+    Преобразует дату из формата 'YYYY-MM-DDTHH:MM:SS.microsecond' в формат 'ДД.ММ.ГГГГ'.
+
+    Аргументы:
+    date_str (str): Строка с датой в формате 'YYYY-MM-DDTHH:MM:SS.microsecond'.
+
+    Возвращает:
+    str: Строка с датой в формате 'ДД.ММ.ГГГГ'.
+    """
+    # Парсим строку даты и времени в объект datetime
+    date_obj = datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%S.%f")
+    # Преобразуем в строку формата "ДД.ММ.ГГГГ"
+    return date_obj.strftime("%d.%m.%Y")
