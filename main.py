2ace40bdd9588be2aa3ff40d852414d87e233fb3
Alex Potemkin <koldunpot98@gmail.com>
2024-10-13 16:54:12 +0700

    Добавлена проверка работоспособности модуля processing.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from src.processing import filter_by_state, sort_by_date
 from src.widget import get_date, mask_account_card
 
 if __name__ == "__main__":
@@ -11,3 +12,13 @@ if __name__ == "__main__":
     print(mask_account_card("Счет 73654108430135874305"))
 
     print(get_date("2024-03-11T02:26:18.671407"))
+
+    list_of_dictionaries = [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+    ]
+
+    print(filter_by_state(list_of_dictionaries, "EXECUTED"))
+    print(sort_by_date(list_of_dictionaries, descending=True))
