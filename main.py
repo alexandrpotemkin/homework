5872bd770bd3437154125c0d562340cf5e43c949
Alex Potemkin <koldunpot98@gmail.com>
2024-10-13 15:12:00 +0700

    Изменил модуль main.py для проверки измененной функции

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from src.widget import get_date, mask_account_card
 
 if __name__ == "__main__":
-    input_str = input("Введите данные карты или счёта: ")
-    print(mask_account_card(input_str))
+    print(mask_account_card("Maestro 1596837868705199"))
+    print(mask_account_card("Счет 64686473678894779589"))
+    print(mask_account_card("MasterCard 7158300734726758"))
+    print(mask_account_card("Счет 35383033474447895560"))
+    print(mask_account_card("Visa Classic 6831982476737658"))
+    print(mask_account_card("Visa Platinum 8990922113665229"))
+    print(mask_account_card("Visa Gold 5999414228426353"))
+    print(mask_account_card("Счет 73654108430135874305"))
 
-    input_str = input("Введите дату в формате 'YYYY-MM-DDTHH:MM:SS.SSSSSS': ")
-    print(get_date(input_str))
+    print(get_date("2024-03-11T02:26:18.671407"))
